fb0aa796c2e578a3bb1cd53143d0e4b38cf20719
githoboman <smithmar681@gmail.com>
2024-08-12 16:19:28 +0100

    commit me

---
+++ my-project/src/Apin.jsx
@@ -0,0 +1,13 @@
+function Apin() {
+    return (
+      <>
+      <div className="bg-image">
+      <h1 className="text-4xl ">
+        Hello world!
+      </h1>
+      </div>
+      </>
+    )
+  }
+
+  export default Apin
---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+    <div className="">
+    <h1 className="text-5xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <button className="text-5l font-bold cursor-pointer mt-7 ">
+      come and see my face
+
+    </button>
+
+    </div>
+    </>
+  )
+}
